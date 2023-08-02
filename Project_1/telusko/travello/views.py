5b8b1ff594b1acc089fbaf998f226b7476ecb22f
Ajay Vishnu E <ajayvishnu121@gmail.com>
2023-08-02 08:50:00 +0530

    db connected

--- Project_1/telusko/travello/views.py
+++ Project_1/telusko/travello/views.py
@@ -7,18 +7,21 @@ def index(request):
     dest1.desc = "The city that never sleeps"
     dest1.img = "destination_1.jpg"
     dest1.price = 900
+    dest1.offer = True
 
     dest2 = Destination()
     dest2.name = "Kolkata"
     dest2.desc = "The city that is always alive"
     dest2.img = "destination_2.jpg"
     dest2.price = 1000
+    dest2.offer = False
 
     dest3 = Destination()
     dest3.name = "Kochi"
     dest3.desc = "The city of love and food"
     dest3.img = "destination_3.jpg"
     dest3.price = 1300
+    dest3.offer = False
 
     dests = [dest1, dest2, dest3]
 
