afb9a0c051154ad82e8bf7abcc10756dd7a20d6c
Ajay Vishnu E <ajayvishnu121@gmail.com>
2023-08-02 12:41:26 +0530

    Login and logout

--- Project_1/telusko/telusko/settings.py
+++ Project_1/telusko/telusko/settings.py
@@ -126,6 +126,9 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media') #to specify media folder
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- Project_1/telusko/travello/views.py
+++ Project_1/telusko/travello/views.py
@@ -2,27 +2,6 @@ from django.shortcuts import render
 from .models import Destination
 
 def index(request):
-    dest1 = Destination()
-    dest1.name = "Mumbai"
-    dest1.desc = "The city that never sleeps"
-    dest1.img = "destination_1.jpg"
-    dest1.price = 900
-    dest1.offer = True
-
-    dest2 = Destination()
-    dest2.name = "Kolkata"
-    dest2.desc = "The city that is always alive"
-    dest2.img = "destination_2.jpg"
-    dest2.price = 1000
-    dest2.offer = False
-
-    dest3 = Destination()
-    dest3.name = "Kochi"
-    dest3.desc = "The city of love and food"
-    dest3.img = "destination_3.jpg"
-    dest3.price = 1300
-    dest3.offer = False
-
-    dests = [dest1, dest2, dest3]
+    dests = Destination.objects.all()
 
     return render(request, 'index.html', {'dests' : dests})
