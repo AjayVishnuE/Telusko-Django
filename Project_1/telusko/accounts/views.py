595766af10cf5daa4a85a6e80f6fdddbd715ee3b
Ajay Vishnu E <ajayvishnu121@gmail.com>
2023-08-03 23:43:12 +0530

    final updates

--- Project_1/telusko/accounts/views.py
+++ Project_1/telusko/accounts/views.py
@@ -38,7 +38,7 @@ def register(request):
                 return redirect('register')
             else:
                 user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
-                user.save();
+                user.save()
                 print('user created')
                 return redirect('login')
         else:
--- Project_1/telusko/travello/views.py
+++ Project_1/telusko/travello/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from .models import Destination
-from django.contrib.auth.models import User, auth
 
 
 def index(request):
@@ -9,9 +8,16 @@ def index(request):
     return render(request, 'index.html', {'dests' : dests})
 
 def getlocationdet(request):
-    if request.method=='GET':
+    if request.method=='POST':
         if request.user.is_authenticated:
-            return render(request, 'loc_details.html')
+            D1=Destination()
+            for i in Destination:
+                if i.name==request.POST['name']:
+                    D1.name=i.name
+                    D1.img=i.img
+                    D1.desc=i.desc
+                    D1.price=i.price
+            return render(request, 'loc_details.html',{'D1':D1})
         else:
             return render(request, 'login.html')
     else:
